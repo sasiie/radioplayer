1f412bc612dc74526f28d63d5bb2649dd5a5657d
sasiie <Sarchiradic99@gmail.com>
2023-11-28 23:12:47 +0100

    klar

--- radioplayer.js
+++ radioplayer.js
@@ -1,55 +1,22 @@
-// Steg 1. Gör en fetch till 'https://api.sr.se/api/v2/channels/?format=json'
-
-// Steg 2. loopa med tex forEach över data.channels - ta ut data och visa på html-sidan.
-
-// Steg 3. ta ut liveaudio.url från varje kanal och lägg i en audio tagg.
-// <audio controls>
-//   <source src="" type="audio/mpeg" />
-// </audio>
 fetch("https://api.sr.se/api/v2/channels/?format=json")
   .then((response) => response.json())
   .then((data) => {
-    const channelsContainer = document.createElement("div");
-    channelsContainer.classList.add("channels-container");
-
     data.channels.forEach((channel) => {
-      const playerDiv = document.createElement("div");
-      playerDiv.classList.add("music-player");
+      const channelName = channel.name;
+      const channelImage = channel.image;
+      const liveAudioUrl = channel.liveaudio.url;
 
-      const image = document.createElement("img");
-      image.src = channel.image;
-      image.alt = channel.name;
-      playerDiv.appendChild(image);
+      const channelElement = document.createElement("div");
+      channelElement.innerHTML = `
 
-      const title = document.createElement("h2");
-      title.textContent = channel.name;
-      playerDiv.appendChild(title);
+        <h2>${channelName}</h2>
+        <img src="${channelImage}" alt="${channelName} Image">
+        <audio controls>
+          <source src="${liveAudioUrl}" type="audio/mpeg" />
+        </audio>
+      `;
 
-      const audio = document.createElement("audio");
-      const source = document.createElement("source");
-      source.src = channel.liveaudio.url;
-      source.type = "audio/mpeg";
-      audio.appendChild(source);
-      playerDiv.appendChild(audio);
-
-      const playButton = document.createElement("button");
-      playButton.textContent = "Play";
-      playButton.addEventListener("click", function () {
-        if (audio.paused) {
-          audio.play();
-          playButton.textContent = "Pause";
-        } else {
-          audio.pause();
-          playButton.textContent = "Play";
-        }
-      });
-      playerDiv.appendChild(playButton);
-
-      channelsContainer.appendChild(playerDiv);
+      channelElement.style.backgroundColor = `#${channel.color}`;
+      document.getElementById("channels").appendChild(channelElement);
     });
-
-    document.body.appendChild(channelsContainer);
-  })
-  .catch((error) => {
-    console.log("Error fetching data:", error);
   });
